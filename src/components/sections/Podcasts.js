e86da78662d7abba31c41831a41a1941231c6bb5
bizounis <bizreb11sc@gmail.com>
2022-06-30 18:58:21 +0300

    update podcast structure

--- src/components/sections/Podcasts.js
+++ src/components/sections/Podcasts.js
@@ -1,22 +1,73 @@
 import React from 'react'
 import PodPreviewSingle from '../PodPreviewSingle'
 import BtnRound from '../BtnRound'
-import cr1 from '../../assets/users/user1.png'
-import cr2 from '../../assets/users/user2.png'
-import cr3 from '../../assets/users/user3.png'
-import cr4 from '../../assets/users/usertennis.png'
-import cr5 from '../../assets/users/userart.png'
-import cr6 from '../../assets/users/userbtc.png'
-import cr7 from '../../assets/users/usercrypto.png'
-import cr8 from '../../assets/users/usergamer.png'
-import cover1 from '../../assets/podcasts/goattalk_cover.png'
-import cover2 from '../../assets/podcasts/artnews_cover.png'
-import cover3 from '../../assets/podcasts/thisweeksmovies_cover.png'
-import cover4 from '../../assets/podcasts/tennis_cover.png'
-import cover5 from '../../assets/podcasts/vinyltalk_cover.png'
-import cover6 from '../../assets/podcasts/crypto_cover.png'
-import cover7 from '../../assets/podcasts/blockchain_cover.png'
-import cover8 from '../../assets/podcasts/gaming_cover.png'
+
+const pods = [
+  {
+    'avatar' : require('../../assets/users/user1.png'),
+    'username' : 'Mike Noble',
+    'cover' : require('../../assets/podcasts/goattalk_cover.png'),
+    'title' : 'Goat Talk',
+    'episodes' : 10,
+    'duration' : 50
+  },
+  {
+    'avatar' : require('../../assets/users/user2.png'),
+    'username' : 'Destiny White',
+    'cover' : require('../../assets/podcasts/artnews_cover.png'),
+    'title' : 'Art News',
+    'episodes' : 10,
+    'duration' : 50
+  },
+  {
+    'avatar' : require('../../assets/users/user3.png'),
+    'username' : 'Victoriahh',
+    'cover' : require('../../assets/podcasts/thisweeksmovies_cover.png'),
+    'title' : 'Movies',
+    'episodes' : 10,
+    'duration' : 50
+  },
+  {
+    'avatar' : require('../../assets/users/usertennis.png'),
+    'username' : 'James Known',
+    'cover' : require('../../assets/podcasts/tennis_cover.png'),
+    'title' : 'Tennis Recap',
+    'episodes' : 10,
+    'duration' : 50
+  },
+  {
+    'avatar' : require('../../assets/users/userart.png'),
+    'username' : 'Anthony Note',
+    'cover' : require('../../assets/podcasts/vinyltalk_cover.png'),
+    'title' : 'Weekly Digs',
+    'episodes' : 10,
+    'duration' : 50
+  },
+  {
+    'avatar' : require('../../assets/users/userbtc.png'),
+    'username' : 'blockworld',
+    'cover' : require('../../assets/podcasts/crypto_cover.png'),
+    'title' : 'Cryptoland',
+    'episodes' : 10,
+    'duration' : 50
+  },
+  {
+    'avatar' : require('../../assets/users/usercrypto.png'),
+    'username' : 'CryptoRami',
+    'cover' : require('../../assets/podcasts/blockchain_cover.png'),
+    'title' : 'Blockchain',
+    'episodes' : 10,
+    'duration' : 50
+  },
+  {
+    'avatar' : require('../../assets/users/usergamer.png'),
+    'username' : '_ricky7_',
+    'cover' : require('../../assets/podcasts/gaming_cover.png'),
+    'title' : 'Gaming News',
+    'episodes' : 10,
+    'duration' : 50
+  },
+]
 
 const Podcasts = () => {
   return (
@@ -24,14 +75,10 @@ const Podcasts = () => {
       <div className='flex flex-col flex-1 justify-center sm:p-20 items-center'>
         <h1 className='text-center mt-8 md:mt-0 mb-4 text-6xl font-bold'>Top Podcasts of the week.</h1>
         <div className='grid w-full sm:grid-cols-2 xl:grid-cols-4 mb-4 '>
-          <PodPreviewSingle creatorAv={cr1} creatorName={'Mike Noble'} podCover={cover1} podName={'Goat Talk'} podEps={'10 Eps'} podDur={'50min'} />
-          <PodPreviewSingle creatorAv={cr2} creatorName={'Destiny White'} podCover={cover2} podName={'Art History'} podEps={'10 Eps'} podDur={'50min'} />
-          <PodPreviewSingle creatorAv={cr3} creatorName={'Victoriahh'} podCover={cover3} podName={'Movies'} podEps={'10 Eps'} podDur={'50min'} />
-          <PodPreviewSingle creatorAv={cr4} creatorName={'James Known'} podCover={cover4} podName={'Tennis Recap'} podEps={'10 Eps'} podDur={'50min'} />
-          <PodPreviewSingle creatorAv={cr5} creatorName={'Anthony Note'} podCover={cover5} podName={'Weekly Digs'} podEps={'10 Eps'} podDur={'50min'} />
-          <PodPreviewSingle creatorAv={cr6} creatorName={'blockworld'} podCover={cover6} podName={'Cryptoland'} podEps={'10 Eps'} podDur={'50min'} />
-          <PodPreviewSingle creatorAv={cr7} creatorName={'CryptoRami'} podCover={cover7} podName={'Blockchain'} podEps={'10 Eps'} podDur={'50min'} />
-          <PodPreviewSingle creatorAv={cr8} creatorName={'_ricky7_'} podCover={cover8} podName={'Gaming News'} podEps={'10 Eps'} podDur={'50min'} />
+          {
+            pods.map((podcast) => 
+              <PodPreviewSingle creatorAv={podcast.avatar} creatorName={podcast.username} podCover={podcast.cover} podName={podcast.title} podEps={podcast.episodes} podDur={podcast.duration}/>)
+          }
         </div>
         <BtnRound text={'More Podcasts'} fill={'nofill'} />
       </div>
